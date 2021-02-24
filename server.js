85cece2f797d001b8c8d1ef9fb96532558befc1d
Andrey Dolya <dolya7kk@gmail.com>
2021-02-24 23:57:22 +0200

    build

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const userRoutes = require('./api/users/user.routes');
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => {
+app.get('/**', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
